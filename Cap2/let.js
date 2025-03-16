a7dc7fadadf76a53ed6015c2d204c46e52b4a730
Francisca <francisca.paredes.pr@gmail.com>
2025-03-16 17:35:53 -0300

    let

---
+++ Cap2/let.js
@@ -0,0 +1,10 @@
+// Declaramos una variable topic con valor JavaScript
+var topic = "JavaScript";    
+
+
+if (topic) {
+    let topic = "React";
+    console.log("block", topic);    // block React
+}
+
+console.log("global", topic);   // global React
